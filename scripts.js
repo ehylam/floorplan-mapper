125b42a9b005d6bf872472ce7c814cd86994c366
ehylam <48621527+ehylam@users.noreply.github.com>
2023-01-06 16:51:53 +1100

    More tweaks

--- scripts.js
+++ scripts.js
@@ -3,8 +3,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.getElementById('file').addEventListener('change', function() {
     if (this.files && this.files[0]) {
       const img = document.getElementById('image');
-
       img.src = URL.createObjectURL(this.files[0]);
+      // on img loaded
+      img.onload = function() {
+        const svg = document.querySelector('.mapper svg');
+        svg.setAttribute('viewBox', `0 0 ${img.width} ${img.height}`);
+      }
     }
   });
 
@@ -23,8 +27,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
     updatePolygon();
   });
 
+
+
   function updatePolygon() {
-    polygon.attr('points', points.map(p => p.join(',')).join(' '));
+    console.log(points.join(' '));
+    polygon.attr('points', points.join(' '));
   }
 
   let selectedVertex = null;
@@ -36,6 +43,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     for (let i = 0; i < points.length; i++) {
       const [px, py] = points[i];
       if (Math.abs(x - px) < 5 && Math.abs(y - py) < 5) {
+        console.log(selectedVertex)
         selectedVertex = i;
         break;
       }
